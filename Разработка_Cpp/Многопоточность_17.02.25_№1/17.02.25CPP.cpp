200c082633f2b21812a1c25c40e1423ecdf6c7c1
Kasey <koitvi4@gmail.com>
2025-02-17 10:32:41 +0500

    Многопоточность в cpp. Первая пара по теме многопоточность. В txt сохранены заметки, в коде можно увидеть пример работы с векторами

---
+++ Разработка_Cpp/Многопоточность_17.02.25_№1/17.02.25CPP.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+void f(vector<int>& num, int start, int end, int& res)
+{
+	res = accumulate(num.begin() + start, num.begin() + end, 0);
+}
+
+int main()
+{
+	srand(time(0));
+	vector<int> num;
+	for (int i = 0; i < 100; i++)
+		num.push_back(rand());
+	vector<int> sum(4, 0);
+	vector<thread> thr;
+	int n = thread::hardware_concurrency();
+	int start = 0, end = 0;
+	for (int i = 0; i < n; i++)
+	{
+		start = i * 25;
+		end = start + 25;
+		thr.emplace_back(f, ref(num), start, end, ref(sum[i]));
+
+		/*thr.emplace_back(f, i);
+		this_thread::sleep_for(chrono::milliseconds(10));*/
+	}
+	for (auto& i : thr)
+		i.join();
+
+	cout << accumulate(num.begin(), num.end(), 0);
+}
