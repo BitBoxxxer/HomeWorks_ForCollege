a77441cce698a5df938dfb3321c1e2205bcac406
Kasey <koitvi4@gmail.com>
2025-02-19 12:30:38 +0500

    CPP работа с много поточностью, блокировки, этапные вызовы зависимых потоков. Новые быблиотеки: <semaphore>, <atomic> [фикс на одном потоке компа.]. Дополнение решения калькулятора flutter с колледжа.

--- flutter_dart/calculator_17_02_25/calculator_college_version/lib/screen.dart
+++ flutter_dart/calculator_17_02_25/calculator_college_version/lib/screen.dart
@@ -21,13 +21,120 @@ class _calculateScreenState extends State<calculateScreen>{
     return Scaffold(
       body: SafeArea(
         child: Column(
-          
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Text(
+                    '$number1$operator$number2'.isEmpty ? '0' :
+                    '$number1$operator$number2',
+                    style: const TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.right,
+                  ),
+                )
+              )
+              ),
+            Wrap(
+                children: Buttn.buttonValues.map(
+                  (value) => SizedBox(
+                    width: value == Buttn.result ? screenSize.width / 2
+                    : screenSize.width / 4,
+
+                    height: screenSize.width / 4,
+                    child: buildButton(value),
+                  )
+                ).toList(),
+              )
+          ],
         ),
 
       ),
 
     );
   }
+  Widget buildButton(value){
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: Material(
+        color: const Color.fromARGB(255, 91, 72, 59),
+        shape: OutlineInputBorder (
+           borderSide: const BorderSide(
+             color: Color.fromARGB(255, 110, 102, 102)
+           ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: InkWell( // Inkwell - кликабельность калькулятора.
+           onTap: () => (
+             onBtnTap(value),
+            
+             ),
+          child: Center(
+            child: Text(
+              value,
+              style: const TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              )
+            ),
+          )
+        )
+      )
+    );
+  }
 
+  void onBtnTap(String value){
+
+    if (value == Buttn.result){
+      calc();
+      return;
+    }
+    if (value == Buttn.clr){
+      clear();
+      return;
+    }
+    if (value == Buttn.del){
+      delete();
+      return;
+    }
+    if (value == Buttn.per){
+      convertPer();
+      return;
+    }
+  }
+
+  void convertPer(){
+    
+  }
+
+  void calc(){
+    setState(() {
+      operator = '+';
+    });
+  }
+
+  void clear(){
+    setState(() {
+      number1 = '';
+      number2 = '';
+      operator = '';
+    });
+  }
+
+  void delete(){
+    if (number2.isNotEmpty){
+      number2 = number2.substring(0, number2.length - 1);
+    }
+    else if (operator.isNotEmpty){
+      operator = '';
+    }
+    else if(number1.isNotEmpty){
+      number1 = number1.substring(0, number1.length - 1);
+    }
+    setState(() {});
+  }
   
 }
---
+++ Разработка_Cpp/19.02.25РаботаСПотоками_Атомарные_semaphore/19.02.25CPP.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <thread>
+//#include <atomic> // атомарные операции [фиксация на операции, блокировка иных потоков]
+#include <mutex>
+#include <vector>
+#include <condition_variable>
+#include <semaphore>
+using namespace std;
+
+atomic <int> c = 0;
+condition_variable cv;
+int finish = 0;
+mutex mtx;
+counting_semaphore<1> semaphore(1);
+
+void f(int i)
+{
+	semaphore.acquire();
+	this_thread::sleep_for(chrono::seconds(2)); // тайминг для синхронизации времени
+	cout << "Now run thread " << i << endl;
+	semaphore.release();
+
+	/*// синтаксис: cv.wait(lock,[],{return (bool условие);} // только bool условие
+	unique_lock<mutex>lock(mtx);
+	cv.wait(lock, [] { return finish; });
+	cout << "Thread run.\n";
+	lock.unlock();*/
+
+
+	/*for (int i = 0; i < 100; i++) // пример работы atomic
+		++c;
+	cout << c << endl;*/
+}
+
+int main()
+{
+	thread t1(f, 1);
+	thread t2(f, 2);
+	t1.join(); t2.join();
+
+	/*vector <thread> vec;
+	for (int i = 0; i <= thread::hardware_concurrency(); i++)
+	{
+
+		if (i == 2)
+			finish = true;
+		else
+			finish = false;
+
+		vec.emplace_back(f);
+		this_thread::sleep_for(chrono::milliseconds(1000));
+		{
+			lock_guard<mutex>lock(mtx);
+		}
+		cv.notify_one();
+	}
+	for (auto& i : vec)
+		i.join();*/
+
+}
