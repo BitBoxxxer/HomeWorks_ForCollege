20a97f284cd9acca65d4af5543194597482d9c13
Kasey <koitvi4@gmail.com>
2025-02-20 23:47:56 +0500

    Обновление калькулятора Flutter-Dart версия колледжа. Добавлено изображение результата вывода кода

--- flutter_dart/calculator_17_02_25/calculator_college_version/lib/screen.dart
+++ flutter_dart/calculator_17_02_25/calculator_college_version/lib/screen.dart
@@ -38,6 +38,8 @@ class _calculateScreenState extends State<calculateScreen>{
                 )
               )
               ),
+              // child: Container(
+              // )
             Wrap(
                 children: Buttn.buttonValues.map(
                   (value) => SizedBox(
@@ -58,25 +60,24 @@ class _calculateScreenState extends State<calculateScreen>{
   }
   Widget buildButton(value){
     return Padding(
-      padding: const EdgeInsets.all(5),
+      padding: const EdgeInsets.all(8),
       child: Material(
-        color: const Color.fromARGB(255, 91, 72, 59),
+        color: const Color.fromARGB(255, 97, 108, 74),
         shape: OutlineInputBorder (
            borderSide: const BorderSide(
              color: Color.fromARGB(255, 110, 102, 102)
            ),
-          borderRadius: BorderRadius.circular(10),
+          borderRadius: BorderRadius.circular(20),
         ),
         child: InkWell( // Inkwell - кликабельность калькулятора.
-           onTap: () => (
+           onTap: () => 
              onBtnTap(value),
-            
-             ),
           child: Center(
             child: Text(
               value,
               style: const TextStyle(
                 fontSize: 25,
+                color: Color.fromARGB(255, 128, 231, 173),
                 fontWeight: FontWeight.bold,
               )
             ),
@@ -104,16 +105,39 @@ class _calculateScreenState extends State<calculateScreen>{
       convertPer();
       return;
     }
-  }
 
-  void convertPer(){
-    
+    appendValue(value);
   }
 
   void calc(){
+    final double num1 = double.parse(number1);
+    final double num2 = double.parse(number2);
+    var result = 0.0;
+
+    switch(operator)
+      {
+        case Buttn.add:
+        result = num1 + num2;
+          break;
+
+        case Buttn.substract:
+        result = num1-num2;
+          break;
+
+        case Buttn.divide:
+        result = num1 / num2;
+          break;
+
+        case Buttn.multiply:
+        result = num1 * num2;
+          break;
+      }
+
     setState(() {
-      operator = '+';
+      number1 = result.toStringAsPrecision(5); // 5 знаков после точко
     });
+    operator = '';
+    number2 = '';
   }
 
   void clear(){
@@ -137,4 +161,45 @@ class _calculateScreenState extends State<calculateScreen>{
     setState(() {});
   }
   
+  void convertPer(){
+    if(number1.isNotEmpty && operator.isNotEmpty && number2.isNotEmpty) {
+      calc();
+    }
+    final number = double.parse(number1);
+    setState(() {
+      number1 = '${(number / 10)}';
+      operator = '';
+      number2 = '';
+    });
+
+  }
+
+  void appendValue(String value){
+    if (value != Buttn.dot && int.tryParse(value) == null)
+    {
+      if (operator.isNotEmpty && number2.isNotEmpty){
+        calc();
+      }
+      operator = value;
+    }
+    else if(number1.isEmpty || operator.isEmpty){
+       // number1.contains(Buttn.dot) Проверка содержимого number1
+      if (value == Buttn.dot && number1.contains(Buttn.dot)) return;
+      if (value == Buttn.dot && (number1.isEmpty || number1 == Buttn.dot)){
+        value = '0,';
+      }
+      number1 += value;
+    }
+    else if(number2.isEmpty || operator.isNotEmpty){
+       // number2.contains(Buttn.dot) Проверка содержимого number2
+      if (value == Buttn.dot && number2.contains(Buttn.dot)) return;
+      if (value == Buttn.dot && (number2.isEmpty || number2 == Buttn.dot)){
+        value = '0,';
+      }
+      number2 += value;
+    }
+    setState(() {});
+  }
+
+
 }
