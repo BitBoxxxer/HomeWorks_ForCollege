131b369541702c3433995e455f9f7c7fb86d25b7
Kasey <koitvi4@gmail.com>
2025-03-12 18:22:07 +0500

    CPP обновление: memory(устранение утечек памяти). Решение задач с чтением txt файлов и преобразованием данных.

---
+++ Разработка_Cpp/10.03.25_МаленькиеЗаданияНаЧтениеФайловИ_ФорматированиеИнфы/10.03.25CPP.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void chunked(const string& filen, int n)
+{
+    ifstream file(filen);
+    if (!file.is_open())
+        cout << "Error opening file." << endl;
+
+    string line, spisok;
+    while (getline(file, line))
+    {
+        for (char i : line)
+        {
+            if (i == ' ')
+                continue;
+            else
+                spisok += i;
+        }
+        int number_N = 1, count = 1;
+
+        cout << '[';
+        for (char i : spisok)
+        {
+            if (number_N < n)
+            {
+                if (number_N == 1)
+                    cout << "['" << i << "',";
+                else
+                    cout << "'" << i << "',";
+                number_N++; count++;
+            }
+            else
+            {
+                if (count == spisok.size())
+                    cout << "'" << i << "']";
+                else
+                    cout << "'" << i << "'],";
+                number_N = 1; count++;
+            }
+        }
+        cout << ']';
+    }
+}
+
+void file_read(const string& filen) 
+{
+    ifstream file(filen);
+    if (!file.is_open())
+        cout << "Error opening file." << endl;
+
+    string target = "anton", line;
+    int line_count = 0;
+
+    while (getline(file, line)) 
+    {
+        int targetIndex = 0;
+        for (char c : line) 
+            if (c == target[targetIndex]) 
+            {
+                targetIndex++;
+                if (targetIndex == target.length()) {
+                    cout << line_count;
+                    break;
+                }
+            }
+        line_count++;
+    }
+    file.close();
+}
+
+int main() 
+{
+    /*string file_name = "holodos.txt";
+    file_read(file_name);*/
+
+    string file_name2 = "chunks.txt"; int n = 3;
+    chunked(file_name2, n);
+}
---
+++ Разработка_Cpp/12.03.25_БорьбаС_УтечкамиПамяти_memory/12.03.25CPP_.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <memory> // new. unique
+using namespace std;
+
+/*int main()
+{
+	unique_ptr<int> o_ptr{};
+	int ind = 0;
+	for (int i = 2; i < 8000; i++)
+	{
+		int k = 0;
+		for (int j = 2; j < i / 2; j++)
+			if (i % j == 0)
+				k++;
+		if ((k == 3))
+			ms_ptr[ind++] = i;
+	}
+	cout << o_ptr;
+}*/
+
+/*int main()
+{
+	int * p_int = new int();
+	unique_ptr<int> p_ptr{p_int};
+
+	cout << "Input number for p_ptr: ";
+	cin >> *p_int;
+	cout << "\nPolychil: " << *p_int;
+
+	cout << "\np_ptr:";
+}*/
+
+/*
+// борьба с утечками памяти: умный указатель.
+int main()
+{
+	int* ptr = new int(), * ptr2 = new int(); ptr = ptr2;
+
+	unique_ptr<int> o{new int(5)}; // ...<int> o{Можно передать тут ему значение.};
+	unique_ptr<int> o2{ new int(15) };
+	// компактное рамположение указателей, что ускоряет работу
+
+	unique_ptr<int[]> ms_ptr{ make_unique<int[]>(10) }; // make_unique авто-инициализация. 
+	//т.е. не нужно ставить * чтобы указать не на муссорный адресс переменной.
+	for (int i = 0; i < 10; i++)
+	{
+		ms_ptr[i] = i * 2;
+		cout << ms_ptr[i] << ' ';
+	}
+}*/
