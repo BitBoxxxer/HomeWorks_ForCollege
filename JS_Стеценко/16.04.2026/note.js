4b877febaf3e131a4f40773384bb2eeaaabe4e40
Kasey <143746353+BitBoxxxer@users.noreply.github.com>
2026-04-16 16:06:08 +0500

    js lesson (16.04.26 - №2)

---
+++ JS_Стеценко/16.04.2026/note.js
@@ -0,0 +1,1 @@
+console.log("Файл (note.js) подключен")
---
+++ JS_Стеценко/16.04.2026/script.js
@@ -0,0 +1,14 @@
+// console.log("Это скрипт JS !")
+
+// //const data = require('C:\\Users\\A\\Desktop\\GITHUB\\HomeWorks_ForCollege\\JS_Стеценко\\16.04.2026\\note.js');
+
+// let str = 'hello world'
+// console.log(str[-3])
+// console.log(str.at(-11))
+// console.log(str.at(0-str.length))
+
+// console.log(str.includes('javascript'));
+// console.log(str.indexOf('erere'));
+
+let str = 'hello world';
+let arr = Array(...str);
